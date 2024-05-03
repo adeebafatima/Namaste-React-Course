7c710e27c2af0e687c426c9c56297fbc1bbfbd0a
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-05-03 17:02:31 +0530

    ep2_JSX

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, 'Heading 1'),
-    React.createElement("h2", {}, 'Heading 2'),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, 'Heading 1'),
-    React.createElement("h2", {}, 'Heading 2'),
-  ]),
-]);
+// JSX => Babel transpile it to React.createElement => ReactElement-JS Object =>HTML Element render
+const jsxHeading = (
+  <h1 id="heading1 " className="head">
+    Hello World JSX
+  </h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
