c6ff0401440780561ef42fd6a2b8e9c522b3bff8
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-05-04 00:42:45 +0530

    ep2_ReactElement+ReactComponent

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Function Expressions are being used but you can use Function Statement as well.
-const Title = function () {
-  return <h1>Lets Learn Functional React Component</h1>;
-};
+// You can put React Element inside React Component and vice versa + You can put React Element inside React Element & React Component inside React Component
 
-const HeadingComponent = function () {
-  return (
-    <div id="container">
-      <Title />
-      <h2 id="heading"> Hello World Functional Component</h2>
-    </div>
-  );
-};
+const Head3 = () => <h3>React Functional Component H3</h3>;
+
+const head2 = <h2>React Element H2</h2>;
+const title = (
+  <div>
+    <h1>React Element H1</h1>
+    {head2}
+    <Head3 />
+  </div>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h4>React Functional Component H4</h4>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
