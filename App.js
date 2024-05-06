e97613cbd228ab600476c536672973f55c790626
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-05-06 16:52:38 +0530

    ep4_FoodApp_StaticBasicStruc

--- App.js
+++ App.js
@@ -25,6 +25,17 @@ const Body = () => (
     <div className="searchBar">Search Bar</div>
     <div className="resContainer">
       <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
     </div>
   </div>
 );
@@ -32,10 +43,16 @@ const Body = () => (
 const RestaurantCard = () => (
   // In-line CSS: In order to write in-line css pass object inside curly brackets to style attribute
   <div className="resCard" style={{ backgroundColor: "orange" }}>
-    <div className="resImage"></div>
-    <div className="resName">
-      <h3>Zyaka Foods</h3>
-    </div>
+    <img
+      className="resImage"
+      alt="res image"
+      src="https://home-cooks.co.uk/cdn/shop/products/chicken-biryani-by-azam-homecooks-981107.jpg"
+    />
+
+    <h3>Zyaka Foods</h3>
+    <h4>Biryani, North Indian, Asian</h4>
+    <h4>4.4 Stars</h4>
+    <h4>38 minutes</h4>
   </div>
 );
 
