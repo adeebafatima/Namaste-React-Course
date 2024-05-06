0ed662c95ed2e7233b5a943f2458a00d4724bf35
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-05-06 14:03:39 +0530

    ep4_FoodApp_Header

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// You can put React Element inside React Component and vice versa + You can put React Element inside React Element & React Component inside React Component
-
-const Head3 = () => <h3>React Functional Component H3</h3>;
-
-const head2 = <h2>React Element H2</h2>;
-const title = (
-  <div>
-    <h1>React Element H1</h1>
-    {head2}
-    <Head3 />
+const Header = () => (
+  <div className="header">
+    <div className="logoConatiner">
+      <img
+        className="logo"
+        src="https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg"
+      />
+    </div>
+    <div className="navItems">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
   </div>
 );
 
-const HeadingComponent = () => (
-  <div id="container">
-    {title}
-    <h4>React Functional Component H4</h4>
+const AppLayout = () => (
+  <div className="appLayout">
+    <Header />
   </div>
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
