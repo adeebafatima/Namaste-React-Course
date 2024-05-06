638d81ccb8a453215fb9f82280a3a490e0e46da9
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-05-06 18:26:45 +0530

    ep4_FoodApp_codeCorr+ObjDestructing

--- App.js
+++ App.js
@@ -1,63 +1,69 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Header = () => (
-  <div className="header">
-    <div className="logoConatiner">
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoConatiner">
+        <img
+          className="logo"
+          src="https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg"
+        />
+      </div>
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div>
+      <div className="searchBar">Search Bar</div>
+      <div className="resContainer">
+        {/* React will take all these properties(resName, cusine) (props) and it will wrap it inside an object and it will pass it to the component as props */}
+        <RestaurantCard resName="Zyaka International" cuisine="Muglai" />
+        <RestaurantCard resName="Megna Foods" cuisine="North Indian" />
+        <RestaurantCard resName="Golappa" cuisine="Chats" />
+        <RestaurantCard resName="Fantastic Ice Cream" cuisine="Ice cream" />
+        <RestaurantCard resName="Zukuba Foods" cuisine="Italian" />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { resName, cuisine } = props;
+  return (
+    <div className="resCard" style={{ backgroundColor: "orange" }}>
       <img
-        className="logo"
-        src="https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg"
+        className="resImage"
+        alt="res image"
+        src="https://home-cooks.co.uk/cdn/shop/products/chicken-biryani-by-azam-homecooks-981107.jpg"
       />
+
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>4.4 Stars</h4>
+      <h4>38 minutes</h4>
     </div>
-    <div className="navItems">
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="appLayout">
+      <Header />
+      <Body />
     </div>
-  </div>
-);
-
-const Body = () => (
-  <div>
-    <div className="searchBar">Search Bar</div>
-    <div className="resContainer">
-      {/* React will take all these properties(resName, cusine) (props) and it will wrap it inside an object and it will pass it to the component as props */}
-      <RestaurantCard resName="Zyaka International" cuisine="Muglai" />
-      <RestaurantCard resName="Megna Foods" cuisine="North Indian" />
-      <RestaurantCard resName="Golappa" cuisine="Chats" />
-      <RestaurantCard resName="Fantastic Ice Cream" cuisine="Ice cream" />
-      <RestaurantCard resName="Zukuba Foods" cuisine="Italian" />
-    </div>
-  </div>
-);
-
-// Destructing on the fly
-const RestaurantCard = ({ resName, cuisine }) => (
-  // or Instead of destructing on the fly we can do:
-  // const {resName,cuisine} = props;
-  <div className="resCard" style={{ backgroundColor: "orange" }}>
-    <img
-      className="resImage"
-      alt="res image"
-      src="https://home-cooks.co.uk/cdn/shop/products/chicken-biryani-by-azam-homecooks-981107.jpg"
-    />
-
-    <h3>{resName}</h3>
-    <h4>{cuisine}</h4>
-    <h4>4.4 Stars</h4>
-    <h4>38 minutes</h4>
-  </div>
-);
-
-const AppLayout = () => (
-  <div className="appLayout">
-    <Header />
-    <Body />
-  </div>
-);
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout />);
