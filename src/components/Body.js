08be2533c8873ff81b1098d7905a1e28ac14f17f
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-06-23 11:13:30 +0530

    ep6_SearchBar

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,13 @@ import RestaurantCard from "./RestaurantCard";
 import { SWIGGY_API } from "../config/constants";
 
 const Body = () => {
+  console.log("Body Rendered!");
   const [listOfRestaurants, setListofRestaurants] = useState([]);
+
+  //IMP:Whenever state variable update ,react triggers a reconcillation cycle(re-renders the component)
+
+  //Whenever you change local state variable react RE-RENDER the component
+  const [searchText, setSearchText] = useState("");
 
   // Page loads -> Render UI(blank since listOfRestaurants is empty) -> API call -> Re-Render API(we set listOfRestaurants using state variable setListOfRestaurants). Since we want to call API after initial render.
   useEffect(() => {
@@ -26,19 +32,37 @@ const Body = () => {
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
-    <div>
-      <div className="searchBar">Search Bar</div>
-      <button
-        className="filter-btn"
-        onClick={() => {
-          const filteredListOfRestaurants = listOfRestaurants.filter(
-            (res) => res.info.avgRating > 4
-          );
-          setListofRestaurants(filteredListOfRestaurants);
-        }}
-      >
-        Top Rated Restaurant
-      </button>
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="searchBar"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              console.log(searchText);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredListOfRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListofRestaurants(filteredListOfRestaurants);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="resContainer">
         {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
