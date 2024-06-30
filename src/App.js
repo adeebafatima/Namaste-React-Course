e5a4cc9afac47dc86bc3b003986ff72b9e94f7a4
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-06-30 18:39:23 +0530

    ep7_Outlet-ChildrenRoutes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -13,7 +13,7 @@ const AppLayout = () => {
   return (
     <div className="appLayout">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -22,16 +22,23 @@ const routingConfig = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+        errorElement: <Error />,
+      },
+    ],
     errorElement: <Error />,
   },
 ]);
