2f53f0642da6134b823506ff9f1bcbfef055f611
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-07-01 21:25:56 +0530

    ep8_ClassBasedComp_updateStateVariable

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
-import User from "./User"
-import UserClass from "./UserClass"
+import User from "./User";
+import UserClass from "./UserClass";
 
 const About = () => (
   <div>
     <h1>We are food ordering app</h1>
     <h2>Try our food! Order quickly</h2>
-    <User name="Adeeba Functional" location="Lucknow Functional" handle="@adeeba Functional"/>
-    <UserClass name="Adeeba Class" location="Lucknow Class" handle="@adeeba Class"/>
+
+    {/* <User
+      name="Adeeba Functional"
+      location="Lucknow Functional"
+      handle="@adeeba Functional"
+    /> */}
+    <UserClass
+      name="Adeeba Class"
+      location="Lucknow Class"
+      handle="@adeeba Class"
+    />
   </div>
 );
 
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,7 +5,6 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      count2: 1,
     };
   }
   render() {
@@ -13,11 +12,24 @@ class UserClass extends React.Component {
     const { count, count2 } = this.state;
     return (
       <div className="userCard">
+        <h2>Count:{count}</h2>
+        <button
+          onClick={() => {
+            // this.state.count = this.state.count+1; NEVER update state variable directly
+
+            //Below code will set state variable and re-render the component
+            this.setState({
+              count: this.state.count + 1,
+            });
+            //Suppose we have have 4 state variable and we want to update 2 of them , we can do that inside same setState and React will update only those 2, without touching other 2.
+            //Whenever this Count button is clicked, react will tigger the reconcillation process, it will find diff between 2 objects , it will update the state and re-render the this.component.
+          }}
+        >
+          Count
+        </button>
         <h3>{name}</h3>
         <p>{location}</p>
         <p>{handle}</p>
-        <p>Count:{count}</p>
-        <p>Count2:{count2}</p>
       </div>
     );
   }
