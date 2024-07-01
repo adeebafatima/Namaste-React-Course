c53966dee9cf6d6fd0cbd665eacfe336e8594f80
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-07-01 20:57:57 +0530

    ep8_ClassBasedComp_creatingStateVariable

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,17 @@
-const User = ({name,location,handle}) => {
+import { useState } from "react";
 
-    return (
-        <div className="userCard">
-            <h3>{name}</h3>
-            <p>{location}</p>
-            <p>{handle}</p>
-        </div>
-    )
-}
+const User = ({ name, location, handle }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
+
+  return (
+    <div className="userCard">
+      <h3>{name}</h3>
+      <p>{location}</p>
+      <p>{handle}</p>
+      <p>Count: {count}</p>
+      <p>Count2: {count2}</p>
+    </div>
+  );
+};
 export default User;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 
-class UserClass extends React.Component{
-
-    constructor(props){
-        super(props)
-    }
-    render(){
-        const {name,location,handle}=this.props
-        return (
-            <div className="userCard">
-            <h3>{name}</h3>
-            <p>{location}</p>
-            <p>{handle}</p>
-        </div>
-
-        )
-    }
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
+  }
+  render() {
+    const { name, location, handle } = this.props;
+    const { count, count2 } = this.state;
+    return (
+      <div className="userCard">
+        <h3>{name}</h3>
+        <p>{location}</p>
+        <p>{handle}</p>
+        <p>Count:{count}</p>
+        <p>Count2:{count2}</p>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
