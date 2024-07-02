ffd217a1876243b9395a27c1d7ec02f1ee4d37f8
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-07-02 12:08:00 +0530

    ep8_ReactLifeCycleMethod_commentInDepth

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import UserClass from "./UserClass";
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log("Parent constructore");
+    console.log("Parent constructor");
   }
 
   componentDidMount() {
@@ -26,7 +26,31 @@ class About extends Component {
         then componentDidMount of second child will be called 
         Render is very fast (Diff between object - Virtual DOM)
         Actually updating the DOM is expensive - so React batch Render and batch Mounting(React Optimization) - This is why React is fast 
-        https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/      
+        
+        Output Expectation - 
+            Parent constructor
+            Parent render
+            First Child constructor
+            First Child render
+            First Child componentDidMount
+            Second Child constructor
+            Second Child render
+            Second Child componentDidMount
+            Parent componentDidMount 
+
+        Actual Output -
+            Parent constructor
+            Parent render
+            First Child constructor
+            First Child render
+            Second Child constructor
+            Second Child render
+                ----DOM UPDATED IN SINGLE BATCH----
+            First Child componentDidMount
+            Second Child componentDidMount
+            Parent componentDidMount 
+            
+            https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/ 
         */}
         <UserClass
           name="First"
