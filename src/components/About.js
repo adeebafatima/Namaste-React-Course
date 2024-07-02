85fc62006c9782a2c31d868508a52132962bf092
Fatima <afatim11@LAMU02D7FFTMD6M.uhc.com>
2024-07-02 13:59:42 +0530

    ep8_apiCallIncomponentDidMount

--- src/components/About.js
+++ src/components/About.js
@@ -4,63 +4,23 @@ import UserClass from "./UserClass";
 class About extends Component {
   constructor(props) {
     super(props);
-    console.log("Parent constructor");
+    // console.log("Parent constructor");
   }
 
   componentDidMount() {
-    console.log("Parent componentDidMount");
+    // console.log("Parent componentDidMount");
   }
 
   render() {
-    console.log("Parent render");
+    // console.log("Parent render");
     return (
       <div>
         <h1>[Class]We are food ordering app</h1>
         <h2>[Class]Try our food! Order quickly</h2>
-
-        {/* React will batch the  Render phase of both child ,
-        1st constructor of first child will call then render of first child will call 
-        then constructor of second child will call then render of second child will call,
-        now Commit phase is batched 
-        componentDidMount of first child will called 
-        then componentDidMount of second child will be called 
-        Render is very fast (Diff between object - Virtual DOM)
-        Actually updating the DOM is expensive - so React batch Render and batch Mounting(React Optimization) - This is why React is fast 
-        
-        Output Expectation - 
-            Parent constructor
-            Parent render
-            First Child constructor
-            First Child render
-            First Child componentDidMount
-            Second Child constructor
-            Second Child render
-            Second Child componentDidMount
-            Parent componentDidMount 
-
-        Actual Output -
-            Parent constructor
-            Parent render
-            First Child constructor
-            First Child render
-            Second Child constructor
-            Second Child render
-                ----DOM UPDATED IN SINGLE BATCH----
-            First Child componentDidMount
-            Second Child componentDidMount
-            Parent componentDidMount 
-            
-            https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/ 
-        */}
         <UserClass
-          name="First"
+          name="Adeeba"
           location="Lucknow Class"
           handle="@adeeba Class"
-        />
-        <UserClass
-          name="Second"
-          location="Hyderabad Class"
-          handle="@fatima Class"
         />
       </div>
     );
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,40 +3,34 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-    };
-    console.log(this.props.name, "Child constructor");
-  }
+    this.state ={
+        userInfo:{
+            name:"Dummy Name",
+            location:"Dummy Location",
+            login:"Dummy Handle"
+        }
+    // console.log(this.props.name, "Child constructor");
+  }}
 
-  componentDidMount() {
-    console.log(this.props.name, "Child componentDidMount");
+  async componentDidMount() {
+    // console.log(this.props.name, "Child componentDidMount");
+    const data = await fetch("https://api.github.com/users/adeebafatima");
+    const jsonData = await data.json();
+    console.log(jsonData);
+    this.setState({
+        userInfo:jsonData
+    })
   }
 
   render() {
-    const { name, location, handle } = this.props;
-    const { count, count2 } = this.state;
-    console.log(this.props.name, "Child render");
+    const { name, location, login,avatar_url} = this.state.userInfo;
+    // console.log(this.props.name, "Child render");
     return (
       <div className="userCard">
-        <h2>Count:{count}</h2>
-        <button
-          onClick={() => {
-            // this.state.count = this.state.count+1; NEVER update state variable directly
-
-            //Below code will set state variable and re-render the component
-            this.setState({
-              count: this.state.count + 1,
-            });
-            //Suppose we have have 4 state variable and we want to update 2 of them , we can do that inside same setState and React will update only those 2, without touching other 2.
-            //Whenever this Count button is clicked, react will tigger the reconcillation process, it will find diff between 2 objects , it will update the state and re-render the this.component.
-          }}
-        >
-          Count
-        </button>
         <h3>{name}</h3>
         <p>{location}</p>
-        <p>{handle}</p>
+        <p>{login}</p>
+        <img src={avatar_url}/>
       </div>
     );
   }
