e70d4aeafddb26b0c05ba5f863ae18a4ae693cbb
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-17 09:57:20 +0530

    ep12_createdSlice+Reducer

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"; //@reduxjs/toolkit has things to do with redux, creating a store is a redux thing
-
-const appStore = configureStore({});
+import cartReducer from './cartSlice'
+const appStore = configureStore({
+    //One BIG reducer, Each slice has its own reducer
+    reducer:{
+        cart:cartReducer
+    }
+});
 
 export default appStore;
---
+++ src/utils/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducer: {
+    addItem: (state, action) => {
+      //Mutating data
+      state.items.push(action.payload);
+    },
+    removeItem: (state) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      state.items.length = 0; //[] Why not?
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
