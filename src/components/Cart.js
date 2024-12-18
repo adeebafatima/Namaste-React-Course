898908679026da96d31716898b1011726600cabc
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-18 08:59:38 +0530

    ep12_ClearCartActionDispatch

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,31 @@
-const Cart = () =>{
-    return <div>Cart</div>
-}
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
+import ItemList from "./ItemList";
+
+const Cart = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="text-bold text-2xl">Cart</h1>
+      <div className="w-6/12 m-auto">
+        {cartItems.length === 0 ? (
+          "Your cart is empty! Add Something ;)"
+        ) : (
+          <button
+            className="m-2 p-2 text-white bg-black rounded-lg"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+        )}
+        <ItemList itemCards={cartItems} />
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
