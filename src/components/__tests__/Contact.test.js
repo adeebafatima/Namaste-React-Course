0717f91496924915484d082c58d6acb31a35f849
Adeeba Fatima <adeebafatima1@gmail.com>
2024-12-24 22:40:19 +0530

    ep13_it_aliasOf_test

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -4,7 +4,7 @@ import Contact from "../Contact";
 
 //describe is used to group test cases, we can nest describe till as many levels as required
 describe("Test Cases for Contact Component", () => {
-  test("Should load Contact component", () => {
+  it("Should load Contact component", () => {
     //In order to test UI component inside react , we need to render the component on jsdom
     render(<Contact />); // Support for the experimental syntax 'jsx' isn't currently enabled | Add @babel/preset-react to the 'presets' section of your Babel config to enable transformation.
     const heading = screen.getByRole("heading");
@@ -12,21 +12,21 @@ describe("Test Cases for Contact Component", () => {
     expect(heading).toBeInTheDocument(); // TypeError: expect(...).toBeInTheDocument is not a function | npm i -D @testing-library/jest-dom
   });
 
-  test("Should check if Submit button is present in Contact Component", () => {
+  it("Should check if Submit button is present in Contact Component", () => {
     render(<Contact />);
     const button = screen.getByText("Submit");
 
     expect(button).toBeInTheDocument();
   });
 
-  test("Should check if Message placeholder is present in Contact Component", () => {
+  it("Should check if Message placeholder is present in Contact Component", () => {
     render(<Contact />);
     const message = screen.getByPlaceholderText("Message");
 
     expect(message).toBeInTheDocument();
   });
 
-  test("Should check if there are 2 input boxes present in Contact Component", () => {
+  it("Should check if there are 2 input boxes present in Contact Component", () => {
     //RENDER
     render(<Contact />);
 
